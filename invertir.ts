b68b3d5a0e3c2a6f50f6b42965ebf1f76594879b
Marcelo Javier García <marcejgarcia@gmail.com>
2024-09-30 13:38:18 -0300

    Update invertir.ts

--- invertir.ts
+++ invertir.ts
@@ -8,20 +8,20 @@ por el usuario
 console.clear();
 import * as fs from 'readline-sync';
 
-//usuario carga arreglo1 
-let tam: number = fs.questionInt("ingrese el tamaño para el arreglo: ")
+//usuario carga arreglo
+let tam: number = fs.questionInt("ingrese el tamaño para el arreglo: ");
 let arreglo: number[] = new Array(tam);
 
 //agrega al array
 
-let long: number = arreglo.length
-
+let long: number = arreglo.length;
 for (let i: number = 0; i < long; i++) {
     arreglo[i] = fs.questionInt("agregue un numero al arreglo: ");
-}
+};
+
 //imprimir invertido
 console.log(arreglo);
 for (let i:number=long ; i > -1; i--) {
-    let acumula: number=arreglo[i]
-    console.log (acumula)
+    let acumula: number=arreglo[i];
+    console.log (acumula);
 };
